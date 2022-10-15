f8aa577f5743142ddc02dc01c3545ed2a6550829
foroughafkhami <you@example.com>
2022-10-15 18:57:28 +0330

    Add input

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int num1 = scanner.nextInt();
+        char op = scanner.next().charAt(0);
+        int num2 = scanner.nextInt();
+        double result;
+    }
+}
